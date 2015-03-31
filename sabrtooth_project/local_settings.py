6f7a7c147e032a8f2aa10fb682f7589cfa179310
Russell Giamarino <iamdangeruss@mac.com>
2015-03-30 21:30:55 -0700

    moved templates to project root

---
+++ sabrtooth_project/local_settings.py
@@ -0,0 +1,20 @@
+DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'SABR',
+#        'USER': 'root',
+#        'PASSWORD': 'm@rl0we',
+#        'HOST': '127.0.0.1',
+#        'PORT': '3306'
+#        
+#    },
+    
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sabr',
+        'USER': 'iamdangeruss',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
