6550efc6e8c4b93ef8f6eb2d13967c3bd68f7b3e
Russell Giamarino <iamdangeruss@mac.com>
2015-04-04 11:08:32 -0700

    changed template location in settings

--- sabrtooth_project/settings.py
+++ sabrtooth_project/settings.py
@@ -14,7 +14,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+TEMPLATE_PATH = os.path.join(BASE_DIR, '../templates')
 STATIC_PATH = os.path.join(BASE_DIR,'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
