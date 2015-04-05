71da0bd021e9d631d0200a2b9c4c84f1621d39e7
Russell Giamarino <iamdangeruss@mac.com>
2015-04-05 11:59:20 -0700

    asdF

--- sabrtooth_project/settings.py
+++ sabrtooth_project/settings.py
@@ -63,6 +63,31 @@ ROOT_URLCONF = 'sabrtooth_project.urls'
 WSGI_APPLICATION = 'sabrtooth_project.wsgi.application'
 
 
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'SABR',
+#        'USER': 'root',
+#        'PASSWORD': 'm@rl0we',
+#        'HOST': '127.0.0.1',
+#        'PORT': '3306'
+#        
+#    },
+    
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sabr',
+        'USER': 'iamdangeruss',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
